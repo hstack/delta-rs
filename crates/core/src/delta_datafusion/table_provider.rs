ad98f556f294d1ad18cb050c4e7ff009a8af33e8
Adrian Tanase <atanase@adobe.com>
2026-02-17 09:40:49 +0200

    [HSTACK] - optimize scan builder, avoid parsing stats (double json ser/de)
    Signed-off-by: Adrian Tanase <atanase@adobe.com>

--- crates/core/src/delta_datafusion/table_provider.rs
+++ crates/core/src/delta_datafusion/table_provider.rs
@@ -417,7 +417,7 @@ impl<'a> DeltaScanBuilder<'a> {
                         .snapshot
                         .log_data()
                         .iter()
-                        .map(|f| f.add_action())
+                        .map(|f| f.add_action_no_stats())
                         .collect_vec();
                     let files_scanned = files.len();
                     (files, files_scanned, 0, None)
@@ -445,7 +445,7 @@ impl<'a> DeltaScanBuilder<'a> {
                         .zip(files_to_prune.iter().cloned())
                     {
                         // prune file based on predicate pushdown
-                        let action = file_view.add_action();
+                        let action = file_view.add_action_no_stats();
                         let num_records = file_view.num_records();
                         if keep {
                             // prune file based on limit pushdown
